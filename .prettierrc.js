69f6b3502d49c9edfae06fbcd71bedeccfb00114
Vlad Iliukha <vlad_iliukha@aleannlab.com>
2023-04-19 10:02:26 +0300

    feat: setup demo

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+};
---
+++ src/data/grid.ts
@@ -0,0 +1,46 @@
+export const GRID = [
+  {
+    image: "img_0.png",
+    label: "Dante",
+  },
+  {
+    image: "img_1.png",
+    label: "Leopardi",
+  },
+  {
+    image: "img_2.png",
+    label: "Shakespeare",
+  },
+  {
+    image: "img_3.png",
+    label: "A. Christie",
+  },
+  {
+    image: "img_4.png",
+    label: "Asimov",
+  },
+  {
+    image: "img_5.png",
+    label: "Aleramo",
+  },
+  {
+    image: "img_6.png",
+    label: "Orwell",
+  },
+  {
+    image: "img_7.png",
+    label: "Pirandello",
+  },
+  {
+    image: "img_8.png",
+    label: "Manzoni",
+  },
+  {
+    image: "img_9.png",
+    label: "Deledda",
+  },
+  {
+    image: "img_10.png",
+    label: "Foscolo",
+  },
+];
